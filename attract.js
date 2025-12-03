cd4651b41f0e33f601cf1726e2053bceb34c2dd7
Ivanimator <ivan.banner@gmail.com>
2025-12-03 12:42:29 -0600

    Add files via upload

---
+++ attract.js
@@ -0,0 +1,39 @@
+// altar.js
+
+const ARTBOARD_NAME = "Attract and Repel";             // <- update if needed
+const STATE_MACHINE_NAME = "State Machine 1"; // <- update if needed
+
+window.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("riveCanvas");
+  if (!canvas) return;
+
+  function resizeCanvas() {
+    const dpr = window.devicePixelRatio || 1;
+    const rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width * dpr;
+    canvas.height = rect.height * dpr;
+  }
+
+  resizeCanvas();
+  window.addEventListener("resize", resizeCanvas);
+
+  const riveInstance = new rive.Rive({
+    src: "riv/test_2511_14_AttractRepel.riv",
+    canvas,
+    autoplay: true,
+    useOffscreenRenderer: true,
+
+    artboard: ARTBOARD_NAME,
+    stateMachines: STATE_MACHINE_NAME,
+    autoBind: true,
+
+    onLoad: () => {
+      riveInstance.resizeDrawingSurfaceToCanvas();
+      // riveInstance.play(...) not needed if state machines drive behavior
+    },
+
+    onError: (e) => {
+      console.error("Rive error:", e);
+    },
+  });
+});
